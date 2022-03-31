fa4005e662bef8de07fd3054ca3e42dbeb8579b7
Angelo Gabriel Vieira Rodrigues <agvr74@gmail.com>
2022-03-31 11:20:55 -0300

    Tela Cadastro em andamento

---
+++ FrontEnd/darede-front/src/pages/telaCadastro.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from '../assets/logo-darede.svg'
+import banner from '../assets/banner-cadastro.svg'
+import tel from '../assets/icon-telefone.svg'
+import globo from '../assets/icon-globo.svg'
+import '../style/telaCadastro.css'
+
+function App() {
+  return (
+    <div class='box-body'>
+      <div class="esquerda">
+        <img src={logo} alt="logo Darede" />
+        <img src={banner} alt="banner cadastro" />
+      </div>
+      <div class="direita">
+        <h1>Cadastro</h1>
+        <form action="submit">
+          <input type="text" placeholder="Nome de Usuário" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+          <button type="button" class="btn-form">Cadastrar</button>
+        </form>
+        <div class="conta">
+          <p>Já possui uma conta?</p> <a href="">Conecte-se.</a>
+        </div>
+        <div class="redirect">
+          <button>
+            <img src={tel} alt="ícone entrar em contato"/>
+            Entre em contato
+          </button>
+          <button>
+            <img src={globo} alt="ícone acesse a darede" />
+            Acesse a Darede
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

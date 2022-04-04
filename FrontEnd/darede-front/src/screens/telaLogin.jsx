e727e2f921ccf84a1cf8ebbe8d185ddbb8e98f98
Daniele Vieira <angelogabrielvr@gmail.comgit remote deletegit reset --hard HEAD~1git status>
2022-04-03 21:37:35 -0300

    Funcionalidades da Tela de Login / Testar

--- FrontEnd/darede-front/src/screens/telaLogin.jsx
+++ FrontEnd/darede-front/src/screens/telaLogin.jsx
@@ -5,6 +5,48 @@ import tel from '../assets/icon-telefone.svg'
 import globo from '../assets/icon-globo.svg'
 import '../style/telaLogin.css'
 
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+      erroMensagem: '',
+      isLoading: false
+    }
+  };
+
+  efetuaLogin = (event) => {
+    event.preventDefault();
+
+    this.setState({ erroMensagem: "", isLoading: true })
+
+    /* alterar url da api*/
+    axios.post('http://localhost:5000/api/login', {
+      email: this.state.email,
+      senha: this.state.senha
+    })
+
+      .then(resposta => {
+        if (resposta.status === 200) {
+          localStorage.setItem('usuario-login', resposta.data.token);
+          this.setState({ isLoading: false });
+          this.props.history.push('/');
+        }
+      })
+      .catch(() => {
+        this.setState({ erroMensagem: "Email e/ou senha inválidos!", isLoading: false });
+      })
+  }
+
+  atualizaStateCampo = (campo) => {
+    this.setState({ [campo.target.name]: campo.target.value })
+  }
+
+}
+
+
 function telaLogin() {
   return (
     <div class="box-bodyL">
@@ -14,11 +56,26 @@ function telaLogin() {
       </div>
       <div class="direitaL">
         <h1>Login</h1>
-        <form action="submit">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
-          <button type="button" class="btn-formL">Login</button>
+        <form action="submit" onSubmit={this.efetuaLogin}>
+
+          <input type="email" placeholder="Email"
+            name="email"
+            value={this.state.email}
+            onChange={this.atualizaStateCampo} />
+
+          <input type="password" placeholder="Senha"
+            name="senha"
+            value={this.state.senha}
+            onChange={this.atualizaStateCampo} />
+
+          <p style={{ color: 'red' }}>{this.state.erroMensagem}</p>
+
+          {
+            this.state.isLoading === false &&
+            <button disabled={this.state.email === '' || this.state.senha === '' ? 'none' : ''} type="button" class="btn-formL">Login</button>
+          }
         </form>
+
         <div class="contaL">
           <p>Não possui uma conta?</p> <a href="">Cadastre-se.</a>
         </div>
@@ -36,5 +93,3 @@ function telaLogin() {
     </div>
   );
 }
-
-export default telaLogin

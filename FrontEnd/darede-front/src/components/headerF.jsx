2c3c489bab65355689e5c079e4824050662609ea
Angelo Gabriel Vieira Rodrigues <agvr74@gmail.com>
2022-05-02 11:13:39 -0300

    Cadastro Infraestrutura Pronto

--- FrontEnd/darede-front/src/components/headerF.jsx
+++ FrontEnd/darede-front/src/components/headerF.jsx
@@ -30,7 +30,45 @@ export default function HeaderF() {
 
                         <Modal>
                             <h1>Cadastrar Infraestrutura</h1>
-                            
+                            <div className="modal-cadastro">
+                                <form action="submit">
+
+                                    <h2>Informações do Cliente</h2>
+                                    <div className="input-group input-group1">
+                                        <input type="text" placeholder="Nome do Cliente" />
+                                        <input type="email" placeholder="Email" />
+                                    </div>
+
+                                    <h2>Informações do Servidor</h2>
+                                    <div className="input-group input-group2">
+                                        <div className="input-group-column">
+                                            <input type="text" placeholder="Memória" />
+                                            <input type="text" placeholder="Armazenamento" />
+                                            <input type="text" placeholder="Processador" />
+                                        </div>
+                                        <div className="input-group-column">
+                                            <input type="text" placeholder="Sistema Operacional" />
+                                            <input type="text" placeholder="Tipo de Instância" />
+                                            <input type="text" placeholder="CPU" />
+                                        </div>
+                                    </div>
+
+                                    <h2>Informações de Rede</h2>
+                                    <div className="input-group input-group2">
+                                        <div className="input-group-column">
+                                            <input type="text" placeholder="Ip Privado" />
+                                            <input type="text" placeholder="Máscara" />
+                                            <input type="text" placeholder="Gateway" />
+                                        </div>
+                                        <div className="input-group-column">
+                                            <input type="text" placeholder="Ip Público" />
+                                            <input type="text" placeholder="Máscara" />
+                                            <input type="text" placeholder="Zona de Disponibilidade" />
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn-formL"/* onClick executar cadastro, se cadastro der certo, fechar modal; senão mostrar mensagem de erro*/ >Cadastrar</button>
+                                </form>
+                            </div>
                         </Modal> : null}
                     <button className='button-header'>
                         <img className='img-header button-img' src={filter} alt="icon-filtrar-por" />
--- FrontEnd/darede-front/src/components/modal.jsx
+++ FrontEnd/darede-front/src/components/modal.jsx
@@ -5,10 +5,13 @@ import '../style/modal.css'
 import closeIcon from '../assets/icon-close.svg';
 
 const Modal = ({ children }) => {
-    return(
+
+    return (
         <div className='modal'>
             <div className='modal-container'>
-                <button className='close' id="close-position"><img src={closeIcon} alt="" className="button-img"/></button>
+                <button className='close' id="close-position">
+                    <img src={closeIcon} alt="" className="button-img" />
+                </button>
                 <div className='modal-content'>{children}</div>
             </div>
         </div>
